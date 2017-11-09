01c76f83d84e4410e58e8e5616359b7f2faffa5a
Adjie Guntoro <adjie.guntoro@ezy.travel>
2017-11-09 17:23:04 +0700

    Commit submit transaction modal

--- modal.php
+++ modal.php
@@ -1,4 +1,4 @@
-  <div class="modal is-active">
+  <div class="modal">
         <div class="modal-background"></div>
         <div class="modal-card">
             <header class="modal-card-head">
@@ -24,6 +24,71 @@
                             <td>Cuci Kering Setrika</td>
                             <td>4 Kg</td>
                             <td>Rp. 24000</td>                        
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                            <tr>
+                                <td colspan='2'><b class='is-pulled-right'>Total</b></td>
+                                <td> <b>Rp. 24000</b></td>                            
+                            </tr>
+                    </tfoot>
+                    </table>
+                </div>
+            </section> 
+            <footer class="modal-card-foot">
+                <a class="button is-success">Simpan</a>
+                <a class="button">Tutup</a>
+            </footer>   
+        </div>
+        <button class="modal-close is-large" aria-label="close"></button>
+    </div>
+
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class='modal-card-title'>Tambah Transaksi #2</p>
+            </header> 
+            <section class="modal-card-body">
+                <div class="content">
+                    <div class="transaksi-info"> 
+                        <table class='table'>
+                            <tr>    
+                                <td>Nama : </td>
+                                <td><input class='input is-small' type='text'/></td>
+                            </tr>
+                            <tr>    
+                                <td>Tanggal Masuk : </td>
+                                <td><input class='input is-small' type='date'/></td>
+                            </tr>
+                            <tr>    
+                                <td>Tanggal Keluar : </td>
+                                <td><input class='input is-small ' disabled type='type' /></td>
+                            </tr>
+                        </table>
+
+                    </div>
+                    <table class='table is-fullwidth'>
+                    <thead>
+                        <tr>
+                            <th>Jenis Layanan</th>
+                            <th>Qty</th>
+                            <th>Jumlah</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class='select'>
+                                <select name="" id="">
+                                    <option value="">Cuci Kering Setrika</option>
+                                </select>
+                                </div>
+                            </td>
+                            <td><input class='input' /></td>
+                            <td>Rp. 24000</td>
+                            <td><button class="button is-small"><i class="fa fa-plus"></i></button></td>                        
                         </tr>
                     </tbody>
                     <tfoot>
